d87fcea93e664488df23cc1da8674a584098b1f7
Goshkalinin <goshkalinin@gmail.com>
2023-10-25 17:08:32 +0400

    nothing new

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-from teletoken import TOKEN
+"""Просто напиши python3 main.py."""
+
 import logging
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
-from telegram.ext import Updater, MessageHandler, Filters
 from dataclasses import dataclass
-from pprint import pprint
-from PIL import Image
+
+from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
+                      KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import Filters, MessageHandler, Updater
 
 from replies import reply_machinen
+from teletoken import TOKEN
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    )
 
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-
+# кажется, это надо заменить на cvs, например
 USERS = {}
 
 
 @dataclass
-class UserRequest:
+class UserRequest(object):
+    """Этот реквест отправим в директус."""
+
     id: str
     chat_id: str
     mail: str
     photo: list
     location: str
 
+
 @dataclass
-class UserInfo:
+class UserInfo(object):
+    """
+    Собираем информацию о пользователе.
+
+    Делаем это на каждое входящее сообщение.
+    """
+
     id: int
     state: str
     mail: str
@@ -31,54 +45,123 @@ class UserInfo:
 
 
 def get_user_info(update):
+    """
+    Проверяем, приходил ли уже пользователь.
+
+    Если не приходил — создаём нового, и записываем.
+
+    Args:
+        update (???): што эта
+
+    Returns:
+        user_id (int): literally tg user id
+    """
     user_id = update.message.from_user.id
 
     if user_id not in USERS:
-        user = UserInfo(id=user_id, state="START", mail="", lang="RU")
+        user = UserInfo(id=user_id, state='START', mail='', lang='RU')
         USERS[user_id] = user
 
     return user_id
 
 
 def generate_keyboard(buttons_list):
+    """
+    Генерируем кнопки для пользователя.
+
+    Если список пустой — удаляем текущую клавиатуру.
+
+    Args:
+        buttons_list (list of lists): список кнопков.
+
+    Returns:
+        reply_markup — разметка кнопок для сообщения.
+    """
     reply_markup = ReplyKeyboardMarkup(
         buttons_list,
         resize_keyboard=True,
-        one_time_keyboard=True,)
+        one_time_keyboard=True,
+        )
     if buttons_list == [[]]:
         reply_markup = ReplyKeyboardRemove()
     return reply_markup
 
 
+def request_donate(txt, button, url, update, context):
+    """
+    Выкатываем пользователю кнопку со ссылкой на донатошну.
+
+    Args:
+        txt (str): потому что разные языки
+        button (str): имя кнопки
+        url (str): финальный урл донатошной
+        update (obj): чо пришло, от кого пришло, тип сообщения, тп.
+        context (obj): объект c методами для чата.
+    """
+    keyboard_buttons = [
+        [InlineKeyboardButton(button, url=url)],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard_buttons)
+
+    update.message.reply_text(txt, reply_markup=reply_markup)
+
+
 def request_geo(txt, btns, update, context):
-    keyboard = [[KeyboardButton(btns, request_location=True)]]
+    """
+    Запрашиваем геолокацию чувака.
+
+    Args:
+        txt (str): потому что разные языки
+        btns (str): список кнопок
+        update (obj): чо пришло, от кого пришло, тип сообщения, тп.
+        context (obj): объект c методами для чата.
+    """
+    request_geo = btns[0]
+    abort = btns[1]
+    keyboard = [[KeyboardButton(request_geo, request_location=True)], [abort]]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=txt,
-        reply_markup=reply_markup)
+        reply_markup=reply_markup,
+        )
 
 
 def send_message(message, buttons, update, context):
+    """
+    Генерируем клаву, отправляем сообщеньку.
+
+    Args:
+        message (str): тушка сообщения
+        buttons (list of lists): список кнопков
+        update (obj): чо пришло, от кого пришло, тип сообщения, тп.
+        context (obj): объект c методами для чата.
+    """
     keyboard = generate_keyboard(buttons)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message, reply_markup=keyboard)
-
-
-def user_lang(user, message):
-    if message in ["EN", "HY", "RU"]:
-        user.lang = message
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=message,
+        reply_markup=keyboard,
+        )
 
 
 def handle_message(update, context):
-    message = update.message
+    """
+    Хендлер сообщений.
 
+    Args:
+        update (obj): чо пришло, от кого пришло, тип сообщения, тп.
+        context (obj): объект c методами для чата.
+    """
+    message = update.message
 
     user_id = get_user_info(update)
     user = USERS[user_id]
 
     print(user.state)
 
-    if message.text in ["EN", "HY", "RU"]:
+    # Эту шляпу надо переписать в стейт-машину!
+    if message.text in {'EN', 'HY', 'RU'}:
         user.lang = message.text
     else:
         reply = reply_machinen(user.state, message, user.lang)
@@ -91,13 +174,17 @@ def handle_message(update, context):
         for command in commands:
             eval(command)
 
-    print(user.state, "\n")
+    print(user.state, '\n')
+
+
 def bot():
+    """Запуск и настройка бота."""
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(MessageHandler(Filters.all, handle_message))
     updater.start_polling()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     bot()
--- replies.py
+++ replies.py
@@ -1,65 +1,301 @@
-''' туть мы храним кнопки и ответы пользователю '''
+"""Тут у нас конечный автомат для робота."""
+from dataclasses import dataclass
 
+
+# пустой набор кнопков
+EMPTY_BTNS = [[]]
+RU_DECLINE_BTN = ['Я передумал']
+
+
+# словарь исходящих сообщений стейта START
+RU_MSGS_START = {
+    '/start': """
+— Привет! Я бот команды YRVN.AM.
+Сообщи мне о проблеме с точкой раздельного сбора, или о появлении новой!
+Ты получишь всю информацию о статусе заявки (если хочешь :))
+""",
+
+    '/point_issue_get_geo': """
+ — Вы хотели сдать мусор, а по адресу никто ничего не принимает? Давайте разбираться.
+""",
+
+    '/new_point': """
+ — Вы нашли классное место? Мы с радостью добавим на карту:
+• ремонт обуви
+• ремонт и пошив одежды (ателье)
+• ремонт разной бытовой техники (не сетевые магазины)
+• приём цветных и черных металлов
+• прием шин и покрышек
+• приём стеклотары
+• прием макулатуры
+
+Если за приём проводится оплата — напишите об этом.
+Ответным сообщением ждём адрес точки, сопроводительную информацию, и фотку.
+Фотку необязательно, но хотелось бы.
+""",
+
+    '/write_us': """
+ — Вот прям сюда и напишите ответным сообщением.
+""",
+
+    '/donate': """
+ — Для того, чтобы дать нам денег, надо перейти на наш сайт. \n Не пугайтесь! Мы бережём персональные данные.
+""",
+
+    '/wrong_value': """
+ — Не могу поддержать разговор на эту тему, увы. Выбери из предложенных кнопок!
+""",
+    }
+
+
+RU_MSGS_POINT_ISSUE_GET_GEO = {
+    '/point_issue_get_photo': """
+ — Отлично! Теперь сделайте фотку точки, и пришлите её.
+""",
+
+'/wrong_value': """
+ — Как будто бы это не координаты.
+""",
+    }
+
+
+RU_BTNS_START = {
+    'main': [['Проблема с точкой', 'Новая точка'], ['Написать нам', 'Донат'], ['EN', 'HY']],
+    }
+
+
+# STATE : {COMMAND (FSM SYMBOL) : [MSG, BTNS, NEW_STATE, FN]}
 RU = {
-    "START": {
-        "/start": [" — Привет! Я бот команды YRVN.AM.\nСообщи мне о проблеме с точкой раздельного сбора, или о появлении новой!\nТы получишь всю информацию о статусе заявки (если хочешь :))",
-        [["Проблема с точкой"],["EN", "HY"]],
-        "START"],
+    # юзер выбрал что-то со стартового экрана
+    'START': {
+        '/start': [
+            RU_MSGS_START['/start'],
+            RU_BTNS_START['main'],
+            'START',
+            ],
 
-        "/point_issue_get_geo": [" — Вы хотели сдать мусор, а по адресу никто ничего не принимает? Давайте разбираться.",
-        [[]],
-        "POINT_ISSUE_GET_GEO",
-        "request_geo(' — Для начала, пришлите геолокацию точки', 'Отправить гео',  update, context)"]},
+        '/point_issue_get_geo': [
+            RU_MSGS_START['/point_issue_get_geo'],
+            EMPTY_BTNS,
+            'POINT_ISSUE_GET_GEO',
+            'request_geo(" — Для начала, пришлите геолокацию точки.", ["Отправить гео", "Я передумал"],  update, context)',
+            ],
 
-    "POINT_ISSUE_GET_GEO": {
-        "/point_issue_get_photo": [" — Отлично! Теперь сделайте фотку точки, и пришлите её.",
-        [[]],
-        "POINT_ISSUE_GET_PHOTO"]},
+        '/new_point': [
+            RU_MSGS_START['/new_point'],
+            RU_BTNS_START['main'],
+            'NEW_POINT',
+            ],
 
-    "POINT_ISSUE_GET_PHOTO": {
-        "/choose_feedback": [" — Осталась самая малость. Прислать ли вам результат обработки заявки?",
-        [["Не надо"], ["На почту"], ["Сюда, в чат"]],
-        "CHOOSE_FEEDBACK"]},
+        '/write_us': [
+            RU_MSGS_START['/write_us'],
+            RU_BTNS_START['main'],
+            'WRITE_US',
+            ],
 
-    "CHOOSE_FEEDBACK": {
-        "/finally": [
-        " — Окс! Ещё заявочку?",
-        [["Проблема с точкой"],["EN", "HY"]],
-        "START"],
+        '/donate': [
+            RU_MSGS_START['/donate'],
+            RU_BTNS_START['main'],
+            'START',
+            'request_donate(" — А там можно выбрать удобный способ задонатить.", "Перейти", "https://yrvn.am/ru/payment/",  update, context)',
+            ],
 
-        "/finally_mail": [
-        " — Тогда пришлите почту.",
-        [[]],
-        "CHOOSE_FEEDBACK"]},
+        '/wrong_value': [
+            RU_MSGS_START['/wrong_value'],
+            RU_BTNS_START['main'],
+            'START',
+            ],
+        },
 
+    # юзер отправил координаты
+    'POINT_ISSUE_GET_GEO': {
+        '/point_issue_get_photo': [
+            RU_MSGS_POINT_ISSUE_GET_GEO['/point_issue_get_photo'],
+            [RU_DECLINE_BTN],
+            'POINT_ISSUE_GET_PHOTO',
+            ],
+
+        '/wrong_value': [
+            RU_MSGS_POINT_ISSUE_GET_GEO['/wrong_value'],
+            EMPTY_BTNS,
+            'POINT_ISSUE_GET_GEO',
+            'request_geo(" — Пришлите геолокацию точки.", ["Отправить гео", "Я передумал"],  update, context)',
+            ],
+        },
+
+    # юзер отправил фото кривой точки
+    'POINT_ISSUE_GET_PHOTO': {
+        '/choose_feedback': [
+            ' — Осталась самая малость. Прислать ли вам результат обработки заявки?',
+            [['Не надо'], ['На почту'], ['Сюда, в чат']],
+            'CHOOSE_FEEDBACK',
+            ],
+
+
+        '/wrong_value': [
+            ' — Как будто бы это не фотка. Сделайте фотку точки, и пришлите её.',
+            [RU_DECLINE_BTN],
+            'POINT_ISSUE_GET_PHOTO',
+            ],
+        },
+
+    # юзер выбрал способ обратной связи
+    'CHOOSE_FEEDBACK': {
+        '/finally': [
+            ' — Окс! Ещё заявочку?',
+            RU_BTNS_START['main'],
+            'START',
+            ],
+
+        '/finally_mail': [
+            ' — Тогда пришлите почту.',
+            EMPTY_BTNS,
+            'GETTING_MAIL',
+            ],
+
+        '/wrong_value': [
+            ' — Просто сделай выбор, умоляю.',
+            [['Не надо'], ['На почту'], ['Сюда, в чат']],
+            'CHOOSE_FEEDBACK',
+            ],
+        },
+
+    # юзер отправил мейл
+    'GETTING_MAIL': {
+        '/finally_mail_error': [
+            ' — Я точно знаю, это — не почта! Давайте попробуем ещё раз.',
+            EMPTY_BTNS,
+            'GETTING_MAIL',
+            ],
+
+        '/finally': [
+            ' — Окс! Ещё заявочку?',
+            RU_BTNS_START['main'],
+            'START',
+            ],
+        },
+
+    # юзер отменил операцию
+    'BACK_TO_THE_ROOTS': {
+        '/back_to_the_roots': [
+            ' — Хорошо, я сбросил все изменения. Но всегда можно начать с чистого листа!',
+            RU_BTNS_START['main'],
+            'START',
+            ],
+        },
+
+    # юзер отправил новую точку
+    'NEW_POINT': {
+        '/new_point_info': [
+            ' - Отлично! Что-то добавим, или отправим?',
+            [['Отправить!'], RU_DECLINE_BTN],
+            'NEW_POINT',
+            ],
+
+        '/confirm': [
+            ' — Принято!',
+            RU_BTNS_START['main'],
+            'START',
+            ],
+        },
+
+    # юзер написал нам
+    'WRITE_US': {
+        '/write_us_add': [
+            ' - Отлично! Что-то добавим, или отправим?',
+            [['Отправить!'], RU_DECLINE_BTN],
+            'WRITE_US',
+            ],
+
+        '/confirm': [
+            ' — Принято!',
+            RU_BTNS_START['main'],
+            'START',
+            ],
+        },
     }
 
 EN = {}
 HY = {}
 
 BUTTONS_TRANSLATOR = (
-    ("/start", "/start"),
-    ("/point_issue_get_geo", "Проблема с точкой"),
-    ("/finally", "Не надо"),
-    ("/finally_mail", "На почту"),
+    ('/start', '/start'),
+    ('/point_issue_get_geo', 'Проблема с точкой'),
+    ('/finally', 'Не надо'),
+    ('/finally_mail', 'На почту'),
+    ('/finally', 'Сюда, в чат'),
+    ('/back_to_the_roots', 'Я передумал'),
+    ('/new_point', 'Новая точка'),
+    ('/donate', 'Донат'),
+    ('/write_us', 'Написать нам'),
+    ('/confirm', 'Отправить!'),
     )
 
-def reply_machinen(state, message, lang):
-    replies = {"EN": EN, "RU": RU, "HY": HY}
 
-    text = message.text
+def is_email(text):
+    """
+    Тупая проверка на мейл.
+
+    Надо бы сделать не тупой.
+
+    Args:
+        text (str): e-mail
+
+    Returns:
+        symbol for FSM
+    """
+    if '@' in text:
+        return '/finally'
+    return '/finally_mail_error'
+
+
+def reply_machinen(state, message, lang):
+    """
+    Функция-переводчик.
+
+    Дело в том, что когда юзер присылает что-то непонятное,
+    нужно как-то это обработать. Пользовательский текст
+    воспринимаем как символ алфавита для КА,
+    дешифруем об BUTTONS_TRANSLATOR
+
+    Args:
+        state (str): текущее состояние юзера
+        message (obj): сообщение от юзера
+        land (str): на каком языке мы общаемся
+
+    Returns:
+        [MSG, BTNS, NEW_STATE, FN]
+    """
+    replies = {'EN': EN, 'RU': RU, 'HY': HY}
+
+    symbol = message.text
     location = message.location
     photos = message.photo
 
+    symbol_matched = False  # Переменная для отслеживания совпадения текста
     for buttons in BUTTONS_TRANSLATOR:
-        if text in buttons:
-            text = buttons[0]
-        elif location is not None:
-            text = "/point_issue_get_photo"
-        elif photos != []:
-            text = "/choose_feedback"
-        elif "@" in text:
-            text = "/finally"
+        if symbol in buttons:
+            symbol = buttons[0]
+            symbol_matched = True
+            break
+    # Проверка наличия геолокации и фотографий только если текст не совпал
+    if not symbol_matched:
+        if location is not None and state == 'POINT_ISSUE_GET_GEO':
+            symbol = '/point_issue_get_photo'
+        elif photos and state == 'POINT_ISSUE_GET_PHOTO':
+            symbol = '/choose_feedback'
+        elif state == 'GETTING_MAIL':
+            symbol = is_email(symbol)
+        elif state == 'WRITE_US':
+            symbol = '/write_us_add'
+        elif state == 'NEW_POINT':
+            symbol = '/new_point_info'
+        else:
+            symbol = '/wrong_value'
 
-    print(text)
-    return replies[lang][state][text]
+    if symbol == '/back_to_the_roots':
+        state = 'BACK_TO_THE_ROOTS'
+
+    print(symbol)
+    print(state)
+    return replies[lang][state][symbol]
